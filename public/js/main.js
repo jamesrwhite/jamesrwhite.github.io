910f26aa254c93f3333913493163f36d563c26d3
James White <james.white.15@gmail.com>
2012-12-27 01:22:35 +0000

    Lots of updates, too many to describe at this time of night.

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+
+	function randomise_background() {
+
+		var background_colours = [
+				"#2C96AF",	// Turquiose
+				"#702F92",	// Purple
+				"#AA2F34",	// Red
+				"#2D44AA",	// Blue
+				"#447C2B"	// Green
+			],
+			$body = $("body");
+
+		// Set the random background colour
+		$body.animate({
+			"background-color": background_colours[
+				Math.round(Math.random() * (background_colours.length - 1))
+			]
+		}, 1500);
+
+		// Fade out and hide the overlay
+		$("#overlay").fadeOut(1500, function() {
+
+			$(this).css({
+				zIndex: -1,
+				display: "none"
+			});
+
+		});
+
+	}
+
+	// Run on page load
+	randomise_background();
+
+});
