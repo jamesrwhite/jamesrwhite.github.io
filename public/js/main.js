ff38e51b0ac4b414fcfb28b3bcb733f3a361e7cb
James White <james.white.15@gmail.com>
2012-12-31 16:08:38 +0000

    Full height pages and using localStorage to store previous background colour values.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,25 @@
 $(window).load(function() {
 
+	function supports_local_storage() {
+
+		try {
+
+			return 'localStorage' in window && window['localStorage'] !== null;
+
+		} catch (e) {
+
+			return false;
+
+		}
+
+	}
+
+	function get_random_index(array_length) {
+
+		return Math.round(Math.random() * (array_length - 1));
+
+	}
+
 	function randomise_background() {
 
 		var background_colours = [
@@ -10,13 +30,45 @@ $(window).load(function() {
 				"#2D44AA",	// Blue
 				"#447C2B"	// Green
 			],
-			$body = $("body");
+			$body = $("body"),
+			random_index;
+
+		// Is localStorage supported by the client?
+		if (supports_local_storage()) {
+
+			var local_storage = window.localStorage;
+
+			// If they don't have a value stored, we can just pick a random one
+			if (localStorage["background_colour"] === undefined) {
+
+				random_index = get_random_index(background_colours.length);
+
+			// If they do have one stored we need to check we aren't showing the same
+			// colour twice!
+			} else {
+
+				do {
+
+					random_index = get_random_index(background_colours.length);
+
+				} while (random_index === localStorage["background_colour"]);
+
+			}
+
+			// Store the value for next time
+			localStorage["background_colour"] = random_index;
+
+		// If they don't support localStorage just pick a random colour and hope for
+		// the best!
+		} else {
+
+			random_index = get_random_index(background_colour.length);
+
+		}
 
 		// Set the random background colour
 		$body.animate({
-			"background-color": background_colours[
-				Math.round(Math.random() * (background_colours.length - 1))
-			]
+			"background-color": background_colours[random_index]
 		}, 1500);
 
 		// Fade out and hide the overlay
