b46daa318d585d2ce862fa128d182f0215bc8fad
James White <james.white.15@gmail.com>
2013-10-12 14:58:40 +0100

    Adding some funky colour animation, experiemnts section w/placeholder and hacky scrollable sections

--- js/main.js
+++ js/main.js
@@ -3,114 +3,93 @@ $(document).ready(function() {
 	'use strict';
 
 	// Detecting Base64 Encoded Image Support
-	(function() {
+	var img = new Image;
 
-		var img = new Image;
+	img.onload = img.onerror = function() {
+		
+		if (img.width !== 1 || img.height !== 1) {
 
-		img.onload = img.onerror = function() {
-			
-			if (img.width !== 1 || img.height !== 1) {
+			$('body img').attr('src', function(index, attr) {
 
-				$('body img').attr('src', function(index, attr) {
+				return $(this).attr('data-fallback-src');
 
-					return $(this).attr('data-fallback-src');
+			});
 
-				});
+		} else {
 
-			} else {
+			document.body.classname += ' no-base64';
 
-				document.body.classname += ' no-base64';
+		}
 
-			}
+	};
 
-		};
+	// Test image to try and load
+	img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
 
-		// Test image to try and load
-		img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+	// Handling the background colour
+	var $body = $('body'),
+		hue = Math.round(Math.random() * 359),
+		saturation = '39%',
+		lightness = '34%';
 
-	}());
+	// Helper to return an incremented HSL value
+	function getColour() {
 
-	function supports_local_storage() {
+		hue++;
+		return 'hsl(' + hue + ', ' + saturation + ', ' + lightness + ')';
 
-		try {
+	}
 
-			return window['localStorage'] !== undefined;
+	// Set the random background colour
+	$body.animate({
+		'background-color': getColour()
+	}, 3000);
 
-		} catch (e) {
+	// Fade out the overlay
+	$('#overlay').fadeOut(3000, function() {
+
+		// Remove it from the layout completely
+		$(this).css({
+			zIndex: -1,
+			display: 'none'
+		});
+
+		// Recursive function to animate background colour
+		function animateColour() {
+
+			$body.animate({
+				'background-color': getColour()
+			}, 100, animateColour);
+
+		}
+
+		animateColour();
+
+	});
+
+	// Hacky way to handle scrolling between sections, should really improve this
+	$('#main-nav li').on('click', function(e) {
+
+		var $a = $(this).find('> a');
+
+		// Ignore the mailto link
+		if ($a.data('scroll') == true) {
+
+			e.preventDefault();
+
+			var target = $a.attr('href');
+			var offsets = {
+				'#about': 0,
+				'#projects': $('#scroll-container').find('> #about').height(),
+				'#experiments': $('#scroll-container').find('> #about').height() + $('#scroll-container').find('> #projects').height()
+			};
+
+			$('#scroll-container').animate({marginTop: '-' + offsets[target] + 'px'}, 500);
 
 			return false;
 
 		}
 
-	}
-
-	function get_random_index(array_length) {
-
-		return Math.round(Math.random() * (array_length - 1));
-
-	}
-
-	function randomise_background() {
-
-		var background_colours = [
-				'rgb(86, 56, 163)', // Purple
-				'rgb(162, 43, 43)', // Red
-				'rgb(56, 82, 163)' // Blue
-			],
-			$body = $('body'),
-			random_index;
-
-		// Is localStorage supported by the client?
-		if (supports_local_storage()) {
-
-			var local_storage = window.localStorage;
-
-			// If they don't have a value stored, we can just pick a random one
-			if (localStorage['background-colour'] === undefined) {
-
-				random_index = get_random_index(background_colours.length);
-
-			// If they do have one stored we need to check we aren't showing the same
-			// colour twice!
-			} else {
-
-				do {
-
-					random_index = get_random_index(background_colours.length);
-
-				} while (random_index == localStorage['background-colour']);
-
-			}
-
-			// Store the value for next time
-			localStorage['background-colour'] = random_index;
-
-		// If they don't support localStorage just pick a random colour and hope for
-		// the best!
-		} else {
-
-			random_index = get_random_index(background_colour.length);
-
-		}
-
-		// Set the random background colour
-		$body.animate({
-			'background-color': background_colours[random_index]
-		}, 1500);
-
-		// Fade out and hide the overlay
-		$('#overlay').fadeOut(1500, function() {
-
-			$(this).css({
-				zIndex: -1,
-				display: 'none'
-			});
-
-		});
-
-	}
-
-	// Run on page load
-	randomise_background();
+	});
 
 });
